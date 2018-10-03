17d2b9d551d5ceaa9b1cbd60253317da43d276dc
pdfarhad <pdcsedu@gmail.com>
2018-10-03 17:00:18 +0600

    parse a list in view

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    data = [1, 2, 3]
+    return render_template("index.html", data=data)
 
 @app.route('/about')
 def about():
