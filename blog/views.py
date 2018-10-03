30fa3344db75cb7f5d37da9851f0152dfde3fcfa
pdfarhad <pdcsedu@gmail.com>
2018-10-03 17:21:10 +0600

    parse a dict in view

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     data = [1, 2, 3]
-    return render_template("index.html", data=data)
+    data_dict = [{"x": 3} , {"x": 5}, {"x": 1}]
+    return render_template("index.html", data=data_dict)
 
 @app.route('/about')
 def about():
